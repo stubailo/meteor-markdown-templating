2a666a9fd4708e9d74047f519c084646de9e7098
Sashko Stubailo <sashko@mit.edu>
2014-10-15 23:38:38 -0700

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Define templates inside .md files.",
-  version: '1.1.1',
+  version: '1.2.0',
   name: "simple:markdown-templating",
   git: "https://github.com/stubailo/meteor-markdown-templating"
 });
