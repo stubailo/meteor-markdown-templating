27f2058666dc334d597dba36bcb273ebc5a8dfe5
Jesse Rosenberger <git@jro.cc>
2017-05-05 14:37:36 +0300

    Bump version to v2.0.2 in preparation for publishing.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Define templates inside .md files.",
-  version: '2.0.1',
+  version: '2.0.2',
   name: "simple:markdown-templating",
   git: "https://github.com/stubailo/meteor-markdown-templating"
 });
